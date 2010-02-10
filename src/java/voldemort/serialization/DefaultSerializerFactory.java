c0b9652efb00f81dc678b552a2d9fed49f830fb1
Antoine Toulme <antoine@lunar-ocean.com>
2010-02-09 23:48:08 -0800

    initial commit for Avro support - generic only for now

--- src/java/voldemort/serialization/DefaultSerializerFactory.java
+++ src/java/voldemort/serialization/DefaultSerializerFactory.java
@@ -19,6 +19,7 @@ package voldemort.serialization;
 import java.util.HashMap;
 import java.util.Map;
 
+import voldemort.serialization.avro.AvroGenericSerializer;
 import voldemort.serialization.json.JsonTypeDefinition;
 import voldemort.serialization.json.JsonTypeSerializer;
 import voldemort.serialization.protobuf.ProtoBufSerializer;
@@ -42,6 +43,7 @@ public class DefaultSerializerFactory implements SerializerFactory {
     private static final String JSON_SERIALIZER_TYPE_NAME = "json";
     private static final String PROTO_BUF_TYPE_NAME = "protobuf";
     private static final String THRIFT_TYPE_NAME = "thrift";
+    private static final String AVRO_GENERIC_TYPE_NAME = "avro-generic";
 
     public Serializer<?> getSerializer(SerializerDefinition serializerDef) {
         String name = serializerDef.getName();
@@ -62,6 +64,8 @@ public class DefaultSerializerFactory implements SerializerFactory {
             return new ProtoBufSerializer<Message>(serializerDef.getCurrentSchemaInfo());
         } else if(name.equals(THRIFT_TYPE_NAME)) {
             return new ThriftSerializer<TBase>(serializerDef.getCurrentSchemaInfo());
+        } else if(name.equals(AVRO_GENERIC_TYPE_NAME)) {
+            return new AvroGenericSerializer(serializerDef.getCurrentSchemaInfo());
         } else {
             throw new IllegalArgumentException("No known serializer type: "
                                                + serializerDef.getName());
---
+++ test/unit/voldemort/serialization/avro/AvroGenericSerializerTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2010 Antoine Toulme
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package voldemort.serialization.avro;
+
+import junit.framework.TestCase;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData.Record;
+import org.apache.avro.util.Utf8;
+
+/**
+ * Tests the serialization using the Avro generic approach.
+ * 
+ * @author antoine
+ * 
+ */
+public class AvroGenericSerializerTest extends TestCase {
+
+    public void testRoundtripAvroWithEnum() throws Exception {
+        String jsonSchema = "{\"name\": \"Kind\", \"type\": \"enum\", \"symbols\": [\"FOO\",\"BAR\",\"BAZ\"]}";
+        AvroGenericSerializer serializer = new AvroGenericSerializer(jsonSchema);
+        byte[] bytes = serializer.toBytes("BAR");
+        assertTrue(serializer.toObject(bytes).equals("BAR"));
+    }
+
+    // this fails with 1.2 but works with the current Avro trunk.
+    // public void testRoundtripAvroWithString() throws Exception {
+    // String jsonSchema = "{\"name\": \"Str\", \"type\": \"String\"}";
+    // AvroGenericSerializer serializer = new AvroGenericSerializer(jsonSchema);
+    // byte[] bytes = serializer.toBytes("BAR");
+    // assertTrue(serializer.toObject(bytes).equals("BAR"));
+    // }
+
+    public void testRoundtripAvroWithGenericRecord() throws Exception {
+        String jsonSchema = "{\"name\": \"Compact Disk\", \"type\": \"record\", "
+                            + "\"fields\": ["
+                            + "{\"name\": \"name\", \"type\": \"string\", \"order\": \"ascending\"}"
+                            + "]}";
+
+        AvroGenericSerializer serializer = new AvroGenericSerializer(jsonSchema);
+        Record record = new Record(Schema.parse(jsonSchema));
+        // we need to use a Utf8 instance to map to a String.
+        record.put("name", new Utf8("Hello"));
+        byte[] bytes = serializer.toBytes(record);
+        assertTrue(serializer.toObject(bytes).equals(record));
+    }
+
+    // does not work yet.
+    // public void testRoundtripAvroWithSpecificRecord() {
+    //
+    // String jsonSchema =
+    // "{\"name\": \"Java type\", \"type\": \"java.util.HashMap\"}";
+    //
+    // AvroSerializer serializer = new AvroSerializer(jsonSchema);
+    // byte[] bytes = serializer.toBytes(new HashMap());
+    // assertTrue(serializer.toObject(bytes).equals(new HashMap()));
+    // }
+
+    // looks like there is a gotcha: we need to use Utf8 for strings.
+    // private class OurRecord {
+    //
+    // private String name;
+    //
+    // public OurRecord(String name) {
+    // this.name = name;
+    // }
+    // }
+}
